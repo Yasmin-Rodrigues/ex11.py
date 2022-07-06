08836c191e9cf2f5132d534dd6da37177e5b5613
Yasmin Rodrigues <yasminrodsil@gmail.com>
2022-07-06 15:24:48 -0300

    first commit

---
+++ ex801.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia um número real qualquer e mostre na tela a sua porção inteira 
+from math import trunc
+num =float(input('Digite um número real:'))
+print('A porção inteira do número {} é: {}' .format(num, trunc(num)))
